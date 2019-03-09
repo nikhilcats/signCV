e15bdf30748c95ff41dff7fd3be1fdc9736f0ef6
daedalus_II <30557998+daedalus1235@users.noreply.github.com>
2019-03-09 16:09:59 -0500

    something something datasets

--- classifier/classifier.py
+++ classifier/classifier.py
@@ -3,13 +3,121 @@ from __future__ import absolute_import, division, print_function
 # TensorFlow and tf.keras
 import tensorflow as tf
 from tensorflow import keras
+tf.enable_eager_execution()
+print(tf.VERSION)
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE
 
 # Helper libraries
 import numpy as np
 import matplotlib.pyplot as plt
-import pathlib
+plt.interactive(False)
+
+from pathlib import *
 
 
+data_root = Path('/home/charles/Downloads/dataset5/A')
+
+import random
+all_image_paths = list(data_root.glob('*/*'))
+all_image_paths = [str(path) for path in all_image_paths]
+random.shuffle(all_image_paths)
+
+image_count = len(all_image_paths)
+print(image_count)
+
+import IPython.display as display
 
 
+def caption_image(image_path):
+    image_rel = Path(image_path).relative_to(data_root)
+    return image_rel
 
+for n in range(3):
+  image_path = random.choice(all_image_paths)
+  display.display(display.Image(image_path))
+  print(caption_image(image_path))
+  print()
+
+label_names = sorted(item.name for item in data_root.glob('*/') if item.is_dir())
+print(label_names)
+
+label_to_index = dict((name, index) for index,name in enumerate(label_names))
+print(label_to_index)
+
+all_image_labels = [label_to_index[Path(path).parent.name]
+                    for path in all_image_paths]
+
+print("First 10 labels indices: ", all_image_labels[:10])
+
+img_path = all_image_paths[0]
+print(img_path)
+
+img_raw = tf.read_file(img_path)
+print(repr(img_raw)[:100]+"...")
+
+def preprocess_image(image):
+  image = tf.image.decode_png(image, channels=3)
+  image = tf.image.resize_images(image, [192, 192])
+  image /= 255.0  # normalize to [0,1] range
+
+  return image
+
+def load_and_preprocess_image(path):
+  image = tf.read_file(path)
+  return preprocess_image(image)
+
+image_path = all_image_paths[0]
+label = all_image_labels[0]
+
+plt.imshow(load_and_preprocess_image(img_path))
+plt.grid(False)
+plt.xlabel(caption_image(img_path))
+plt.title(label_names[label].title())
+print()
+
+path_ds = tf.data.Dataset.from_tensor_slices(all_image_paths)
+print('shape: ', repr(path_ds.output_shapes))
+print('type: ', path_ds.output_types)
+print()
+print(path_ds)
+
+image_ds = path_ds.map(load_and_preprocess_image, num_parallel_calls=AUTOTUNE)
+
+plt.figure(figsize=(8,8))
+for n,image in enumerate(image_ds.take(4)):
+  plt.subplot(2,2,n+1)
+  plt.imshow(image)
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+  plt.xlabel(caption_image(all_image_paths[n]))
+
+label_ds = tf.data.Dataset.from_tensor_slices(tf.cast(all_image_labels, tf.int64))
+
+image_label_ds = tf.data.Dataset.zip((image_ds, label_ds))
+print('image shape: ', image_label_ds.output_shapes[0])
+print('label shape: ', image_label_ds.output_shapes[1])
+print('types: ', image_label_ds.output_types)
+print()
+print(image_label_ds)
+
+ds = tf.data.Dataset.from_tensor_slices((all_image_paths, all_image_labels))
+
+# The tuples are unpacked into the positional arguments of the mapped function
+def load_and_preprocess_from_path_label(path, label):
+  return load_and_preprocess_image(path), label
+
+image_label_ds = ds.map(load_and_preprocess_from_path_label)
+image_label_ds
+
+
+BATCH_SIZE = 32
+
+# Setting a shuffle buffer size as large as the dataset ensures that the data is
+# completely shuffled.
+ds = image_label_ds.apply(
+  tf.data.experimental.shuffle_and_repeat(buffer_size=image_count))
+ds = ds.batch(BATCH_SIZE)
+ds = ds.prefetch(buffer_size=AUTOTUNE)
+print(ds)
